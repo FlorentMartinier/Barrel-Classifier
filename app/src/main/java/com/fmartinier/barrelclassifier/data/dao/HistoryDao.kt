c767120fbaea051cfae0fe3219356041cf39ddd4
Florent Martinier <florent.martinier@free.fr>
2025-12-22 21:13:51 +0100

    Release 1.1

--- app/src/main/java/com/fmartinier/barrelclassifier/data/dao/HistoryDao.kt
+++ app/src/main/java/com/fmartinier/barrelclassifier/data/dao/HistoryDao.kt
@@ -25,30 +25,6 @@ class HistoryDao(private val dbHelper: DatabaseHelper) {
         return db.insert(HISTORY_TABLE_NAME, null, values)
     }
 
-    fun getByBarrelId(barrelId: Long): List<History> {
-        val list = mutableListOf<History>()
-        val db = dbHelper.readableDatabase
-
-        val cursor = db.rawQuery(
-            "SELECT * FROM $HISTORY_TABLE_NAME WHERE $BARREL_ID_COLUMN_NAME = ?",
-            arrayOf(barrelId.toString())
-        )
-
-        while (cursor.moveToNext()) {
-            list.add(
-                History(
-                    id = cursor.getLong(0),
-                    barrelId = cursor.getLong(1),
-                    name = cursor.getString(2),
-                    beginDate = cursor.getLong(3),
-                    endDate = cursor.getLong(4)
-                )
-            )
-        }
-        cursor.close()
-        return list
-    }
-
     fun deleteHistory(id: Long) {
         dbHelper.writableDatabase.delete(
             HISTORY_TABLE_NAME,
