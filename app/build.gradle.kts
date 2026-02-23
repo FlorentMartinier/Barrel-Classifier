dc796b955451eeb14371482bd96fb5681f0f833f
Florent Martinier <florent.martinier@free.fr>
2026-02-23 21:31:20 +0100

    Fix icon color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fmartinier.barrelclassifier"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.3.3"
+        versionCode = 20
+        versionName = "2.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
