fb220b386559c923b8499e2000f95d523fb47d12
Florent Martinier <florent.martinier@free.fr>
2026-03-01 23:22:15 +0100

    Release 2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fmartinier.barrelclassifier"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.5.0"
+        versionCode = 23
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
